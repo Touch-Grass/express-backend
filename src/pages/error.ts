96b4451b52892c5fa7b89b55a2295c8091b8b4a4
Touch Grass <toberock101@gmail.com>
2023-01-07 10:01:40 -0800

    Fixed package.json main path

--- src/pages/error.ts
+++ src/pages/error.ts
@@ -1,9 +1,11 @@
 import { app } from '../app';
 
+// 404 err
 app.use((_, res) => {
   res.status(404).send("Sorry can't find that!");
 });
 
+// 505 err
 app.use((_, res) => {
   res.status(500).send("Sorry can't find that!");
 });
